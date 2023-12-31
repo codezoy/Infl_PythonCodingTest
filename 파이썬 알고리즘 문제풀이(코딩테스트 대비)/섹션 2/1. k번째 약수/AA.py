879e3ec27a657a1dcc260544b0a95d76d017359d
Codezoy <pohanes@nave.com>
2023-12-31 10:55:23 +0900

    2. K번째 수

---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 2/1. k번째 약수/AA.py
@@ -0,0 +1,17 @@
+import sys
+# sys.stdin = open('input.txt', 'rt')
+# Question: std.stdin이 뭐야?
+# 
+# n = input()
+N, K = map(int, input().split(' '))
+
+divisor = []
+i = 1
+for i in range(1, N+1):
+    if N%i==0:
+        divisor.append(i)
+
+if len(divisor)<K:
+    print("-1")
+else:
+    print(divisor[K-1])
